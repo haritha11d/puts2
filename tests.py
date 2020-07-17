6c527f8013537e0a53027cadf6b0533ee3327b79
haritha11d <rahi17@student.bth.se>
2020-07-17 21:25:01 +0200

    Add empty page testing

--- tests.py
+++ tests.py
@@ -1,8 +1,19 @@
 import unittest
 import main
 
+
 class TestCalculatorWebApp(unittest.TestCase):
 
     def setUp(self) -> None:
         main.app.testing = True
         self.app = main.app.test_client()
+
+    def testEmptyPage(self):
+        """Test the page with an empty route"""
+
+        response = self.app.get("/")
+        self.assertEqual(b'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n', response.data)
+
+
+
+
