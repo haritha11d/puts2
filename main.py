3c94c585f8a980512d0e3de8a756b7d353f81f44
haritha11d <rahi17@student.bth.se>
2020-07-17 21:51:49 +0200

    Add input handling :woman_technologist:

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
+def input_handling():
+    if request.method == 'GET':
+        values = request.args.get('X', default=0, type=str)
+    else:
+        values = request.values.get('X', default=0, type=str)
+
+
 @app.route('/')
 def index():
     return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
