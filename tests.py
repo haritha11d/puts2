fb18aa6bf60fcf146a7d4b131ae310b9463c59ad
haritha11d <rahi17@student.bth.se>
2020-07-17 21:19:44 +0200

    Initial Commit

---
+++ tests.py
@@ -0,0 +1,8 @@
+import unittest
+import main
+
+class TestCalculatorWebApp(unittest.TestCase):
+
+    def setUp(self) -> None:
+        main.app.testing = True
+        self.app = main.app.test_client()
