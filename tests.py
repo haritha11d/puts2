f1d26a9f5878d753a71a3a461a8bc2412ffee372
haritha11d <rahi17@student.bth.se>
2020-07-17 23:44:09 +0200

    Add tests for mean module for all the routes avg, mean, average

--- tests.py
+++ tests.py
@@ -8,7 +8,12 @@ class TestCalculatorWebApp(unittest.TestCase):
         main.app.testing = True
         self.app = main.app.test_client()
 
+    def testAverage(self):
+        response = self.app.get("/mean?X=1,2,3")
+        self.assertEqual(b'2 \n', response.data)
 
+        response = self.app.get("/avg?X=1,2,3")
+        self.assertEqual(b'2 \n', response.data)
 
-
-
+        response = self.app.get("/average?X=1,2,3")
+        self.assertEqual(b'2 \n', response.data)
