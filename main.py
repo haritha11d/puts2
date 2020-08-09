d0a7cf6ac4d3a36ee03b396869523b6e964213cc
haritha11d <rahi17@student.bth.se>
2020-08-09 20:00:46 +0200

    Add new tests

--- main.py
+++ main.py
@@ -14,13 +14,18 @@ def input_handling():
     try:
         values = [Fraction(value) for value in values.split(',')]
     except ValueError:
-        warning = "Something wrong! --> input should consist of a vector of real numbers."
+        warning = "Something wrong! --> input should consist of a vector of real numbers.\n"
         return warning
-
+    except TypeError:
+        warning = "Something wrong! --> input should consist of a vector of real numbers.\n"
+        return warning
+    except Exception as e:
+        exception = str(e)
+        return exception
     return values
 
 
-@app.route('/median', methods=['GET', 'POST'])
+@app.route('/median', methods=['POST', 'GET'])
 def median():
     try:
         values = input_handling()
@@ -28,8 +33,15 @@ def median():
     except ValueError:
         warning = input_handling()
         return warning
+    except TypeError:
+        warning = input_handling()
+        return warning
+    except Exception as e:
+        exception = input_handling()
+        return exception
     else:
         if float(answer).is_integer():
             answer = int(answer)
             return "%d \n" % answer
         return str(float(round(answer, 3))) + " \n"
+
--- tests.py
+++ tests.py
@@ -4,10 +4,23 @@ import main
 
 class TestCalculatorWebApp(unittest.TestCase):
 
-    def setUp(self) -> None:
+    def setUp(self):
         main.app.testing = True
         self.app = main.app.test_client()
 
-    def testMedian(self):
+    def testAverage(self):
+        response = self.app.get("/median?X=1,2,3")
+        self.assertEqual(b'2 \n', response.data)
+
+        response = self.app.get("/median?X=1000,2.5,100")
+        self.assertEqual(b'100 \n', response.data)
+
+        response = self.app.get("/median?X=-1.2,-2.5,-100,-9292,-0303,0293/92929")
+        self.assertEqual(b'-51.25 \n', response.data)
+
         response = self.app.get("/median?X=1,2,5,0,100,-100,-5,-2")
         self.assertEqual(b'0.5 \n', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
