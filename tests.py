4e8e6c8bd3afb9a5e79f8036da80fb169f9ea9b5
haritha11d <rahi17@student.bth.se>
2020-08-09 18:47:45 +0200

    Add an edge case for error handling

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import main
 
 class TestCalculatorWebApp(unittest.TestCase):
 
-    def setUp(self) -> None:
+    def setUp(self):
         main.app.testing = True
         self.app = main.app.test_client()
 
@@ -27,4 +27,12 @@ class TestCalculatorWebApp(unittest.TestCase):
         response = self.app.get("/mean?X=1000,2.5,100")
         self.assertEqual(b'367.5 \n', response.data)
 
+        """ Edge Case Testing """
 
+        response = self.app.get("/mean?X=x")
+        self.assertEqual(b'Something wrong! --> input should consist of a vector of real numbers.\n',
+                         response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
